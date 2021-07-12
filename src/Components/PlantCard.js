a78e1b90be009e5d714bd1622491e1397705c1dd
spoilaro <johannes.haapanen@gmail.com>
2021-07-12 22:45:25 +0300

    Added plant grid

---
+++ src/Components/PlantCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function PlantCard() {
+  return (
+    <div className="plant-card">
+      <div className="plant-pic"></div>
+      <div className="plant-info">
+        <h3>Bonsai</h3>
+        <h4>OLOHUONE</h4>
+      </div>
+    </div>
+  );
+}
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { AiOutlineSearch } from "react-icons/ai";
+import PlantCard from "../Components/PlantCard";
 
 export default function Home() {
+  const plantList = ["Bonsai", "Sunflower", "Rose", "Cactus", "Baby"];
+
+  const plants = plantList.map((item, index) => (
+    <li>
+      <PlantCard />
+    </li>
+  ));
+
   return (
     <div id="home">
       <header>
         <h1>Plant Care</h1>
         <SearchBar />
       </header>
+
+      <ul className="plant-grid">{plants}</ul>
     </div>
   );
 }
